b7596eb516e8d0281678aae35454aba6a193fd66
Kyle Mellander <kylemellander@gmail.com>
2021-04-13 15:45:01 -0700

    clean up filtering cards

--- dist/index.js
+++ dist/index.js
@@ -11,19 +11,20 @@ const fetch = __nccwpck_require__(467);
 
 async function run() {
   try {
+    const response = await fetch(`https://api.trello.com/1/boards/AY19B6gE/cards?key=${core.getInput("trello_key")}&token=${core.getInput("trello_token")}&attachments=true`)
+    const cards = await response.json()
+    const filteredCards = cards.filter(card => card.attachments.some(attachment => attachment.url.includes("github.com")))
+    core.info("filtering cards")
+    core.info(JSON.stringify(filteredCards, undefined, 2))
+    core.info(filteredCards.length)
     const currentSha = github.context.sha
     const githubToken = core.getInput("github_token")
     const octokit = github.getOctokit(githubToken)
-    const response = await fetch(`https://api.trello.com/1/boards/AY19B6gE/cards?key=${core.getInput("trello_key")}&token=${core.getInput("trello_token")}&attachments=true`)
-    const cards = await response.json()//.filter(card => card.attachments.some(attachment => attachment.url.include("github.com")))
-    // core.info("filtering cards")
-    core.info(typeof cards)
-    core.info(JSON.stringify(cards, undefined, 2))
     const owner = github.context.payload.repository.owner.name
     const repo = github.context.payload.repository.name
     const result = await octokit.rest.repos.listCommits({ owner, repo, sha: currentSha })
     core.info(JSON.stringify(result, undefined, 2))
-    core.setOutput('time', JSON.stringify(result, undefined, 2));
+    core.setOutput('time', result);
   } catch (error) {
     core.setFailed(error.message);
   }
