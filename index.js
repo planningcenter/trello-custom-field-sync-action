1ead983d2ce061ac83b1327ad887690c532a2176
Kyle Mellander <kylemellander@gmail.com>
2021-04-13 14:12:12 -0700

    guard against rrep not existing

--- index.js
+++ index.js
@@ -8,7 +8,8 @@ async function run() {
     const githubToken = core.getInput("github_token")
     const octokit = github.getOctokit(githubToken)
     core.debug(JSON.stringify(github.context.payload, undefined, 2))
-    const [owner, repo] = github.repository.split("/")
+    core.debug(github.repository)
+    const [owner, repo] = (github.repository || "/").split("/")
     const result = await octokit.rest.repos.listCommits({ owner, repo, sha: currentSha })
     core.debug(JSON.stringify(result, undefined, 2))
     core.setOutput('time', repo);
