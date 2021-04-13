933b95f0beac78a2ecabc20417d365cf140bc6e7
Kyle Mellander <kylemellander@gmail.com>
2021-04-13 14:10:09 -0700

    commit changes

--- dist/index.js
+++ dist/index.js
@@ -13,11 +13,11 @@ async function run() {
     const currentSha = github.sha
     core.debug(currentSha)
     const githubToken = core.getInput("github_token")
-    // const octokit = github.getOctokit(githubToken)
+    const octokit = github.getOctokit(githubToken)
     core.debug(JSON.stringify(github.context.payload, undefined, 2))
     const [owner, repo] = github.repository.split("/")
-    // const result = await octokit.rest.repos.listCommits({ owner, repo, sha: currentSha })
-    // core.debug({ result: JSON.stringify(result, undefined, 2) })
+    const result = await octokit.rest.repos.listCommits({ owner, repo, sha: currentSha })
+    core.debug(JSON.stringify(result, undefined, 2))
     core.setOutput('time', repo);
   } catch (error) {
     core.info({ error })
