3f5d59787a32d674fd0c14528bf5385584742421
Kyle Mellander <kylemellander@gmail.com>
2021-04-13 12:02:27 -0700

    add basic finding the previous sha's

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const core = require('@actions/core');
 const github = require("@actions/github");
 
-// most @actions toolkit packages have async methods
 async function run() {
   try {
-    core.info(JSON.stringify(github.context.payload, undefined, 2))
-    core.setOutput('time', new Date().toTimeString());
+    const currentSha = github.sha
+    const githubToken = core.getInput("github_token")
+    const octokit = github.getOctokit(githubToken)
+    const result = octokit.commits(github.repository, { sha: currentSha })
+    core.info(JSON.stringify(result, undefined, 2))
+    core.setOutput('time', result);
   } catch (error) {
     core.setFailed(error.message);
   }
