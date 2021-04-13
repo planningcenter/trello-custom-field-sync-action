5f1035d26e8862b6ed301486a9b36ace709cb5a1
Kyle Mellander <kylemellander@gmail.com>
2021-04-13 14:16:39 -0700

    log better errors

--- index.js
+++ index.js
@@ -4,17 +4,16 @@ const github = require("@actions/github");
 async function run() {
   try {
     const currentSha = github.sha
-    core.debug(currentSha)
+    core.info(currentSha)
     const githubToken = core.getInput("github_token")
     const octokit = github.getOctokit(githubToken)
-    core.debug(JSON.stringify(github.context.payload, undefined, 2))
-    core.debug(github.repository)
+    core.info(JSON.stringify(github.context.payload, undefined, 2))
+    core.info(github.repository)
     const [owner, repo] = (github.repository || "/").split("/")
     const result = await octokit.rest.repos.listCommits({ owner, repo, sha: currentSha })
-    core.debug(JSON.stringify(result, undefined, 2))
+    core.info(JSON.stringify(result, undefined, 2))
     core.setOutput('time', repo);
   } catch (error) {
-    core.info({ error })
     core.setFailed(error.message);
   }
 }
