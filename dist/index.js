89f1cd839a5c7cf2e774caab4a0ec2d746248d82
Kyle Mellander <kylemellander@gmail.com>
2021-04-13 14:26:32 -0700

    fetch actual owner and repo

--- dist/index.js
+++ dist/index.js
@@ -16,7 +16,9 @@ async function run() {
     const octokit = github.getOctokit(githubToken)
     core.info(JSON.stringify(github.context.payload, undefined, 2))
     core.info(github.context.repository)
-    const [owner, repo] = (github.context.repository || "/").split("/")
+    const owner = github.context.payload.repository.owner.name
+    const repo = github.context.payload.repository.name
+    // const [owner, repo] = (github.context.repository || "/").split("/")
     const result = await octokit.rest.repos.listCommits({ owner, repo, sha: currentSha })
     core.info(JSON.stringify(result, undefined, 2))
     core.setOutput('time', repo);
