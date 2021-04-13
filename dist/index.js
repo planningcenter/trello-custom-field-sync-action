dbf055ae2bc7b3af85c2efe730d6f7c0eac3e090
Kyle Mellander <kylemellander@gmail.com>
2021-04-13 14:01:23 -0700

    update debug

--- dist/index.js
+++ dist/index.js
@@ -13,7 +13,7 @@ async function run() {
     const currentSha = github.sha
     const githubToken = core.getInput("github_token")
     // const octokit = github.getOctokit(githubToken)
-    core.debug({ payload: github.context.payload })
+    core.debug(JSON.stringify(github.context.payload, undefined, 2))
     const [owner, repo] = github.repository.split("/")
     // const result = await octokit.rest.repos.listCommits({ owner, repo, sha: currentSha })
     // core.debug({ result: JSON.stringify(result, undefined, 2) })
