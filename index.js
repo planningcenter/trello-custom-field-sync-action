01db7f66505644447bf136e14a3684f21cfcc464
Kyle Mellander <kylemellander@gmail.com>
2021-04-13 13:21:20 -0700

    add debug statements

--- index.js
+++ index.js
@@ -6,8 +6,9 @@ async function run() {
     const currentSha = github.sha
     const githubToken = core.getInput("github_token")
     const octokit = github.getOctokit(githubToken)
-    const result = await octokit.commits(github.repository, { sha: currentSha })
-    core.info(JSON.stringify(result, undefined, 2))
+    core.debug({ payload: github.context.payload })
+    const result = await octokit.commits.commits(github.repository, { sha: currentSha })
+    core.debug({ result: JSON.stringify(result, undefined, 2) })
     core.setOutput('time', result);
   } catch (error) {
     core.setFailed(error.message);
