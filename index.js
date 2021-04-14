c4fca924254a32ab3dda6804cca51468df770ee3
Kyle Mellander <kylemellander@gmail.com>
2021-04-14 14:50:13 -0700

    refactor some of the code to help think about how to proceed

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 const core = require("@actions/core")
 const github = require("@actions/github")
-const { trelloFetch } = require("./trelloUtils")
+const {
+  getCards,
+  getCustomField,
+  getCardCustomItemFields,
+  updateCustomField,
+} = require("./trelloRequests")
+const { getHeadCommitShaForPR, getCommitsFromMaster } = require("./githubRequests")
+const { log } = require("./utils/log")
 
 async function run() {
   try {
     const commits = await findCommitsFromShaToMaster()
     const stagingCustomFieldItem = await getStagingCustomFieldItem()
-    const filteredCards = await getCardsWithPRAttachments()
-    filteredCards.forEach(async (card) => {
+    const cardsWithPRAttached = await getCardsWithPRAttached()
+
+    cardsWithPRAttached.forEach(async (card) => {
       setCardCustomFieldValue({
         card,
         commits,
@@ -15,29 +23,20 @@ async function run() {
       })
     })
   } catch (error) {
-    log("FAILED: " + error.message)
+    log(error)
     core.setFailed(error.message)
   }
 }
 
 run()
 
-async function getCardsWithPRAttachments() {
-  const response = await trelloFetch(
-    `boards/${core.getInput("trello_board_id")}/cards?attachments=true`,
-  )
-  const cards = await response.json()
+async function getCardsWithPRAttached() {
+  const cards = await getCards()
   return cards.filter((card) => card.attachments.some(isPullRequestAttachment))
 }
 
-async function getEnvironmentCustomField() {
-  const response = await trelloFetch(`boards/${core.getInput("trello_board_id")}/customFields`)
-  const customFields = await response.json()
-  return customFields.find(({ name }) => name === core.getInput("trello_custom_field_name"))
-}
-
 async function getStagingCustomFieldItem() {
-  const customField = await getEnvironmentCustomField()
+  const customField = await getCustomField()
   return customField.options.find(
     (option) => Object.values(option.value)[0] === core.getInput("trello_custom_field_value"),
   )
@@ -47,93 +46,40 @@ async function setCardCustomFieldValue({ card, commits, customFieldItem }) {
   const attachments = card.attachments.filter(isPullRequestAttachment)
   const attachment = attachments[0] // TODO: support multiple PR attachments
   const prId = attachment.url.split("/").pop()
-  const headCommitSha = await getHeadCommitForPR(prId)
+  const headCommitSha = await getHeadCommitShaForPR(prId)
   const attachmentIsAMatchedPR = commits.some((commit) => commit.sha === headCommitSha)
   const body = attachmentIsAMatchedPR ? { idValue: customFieldItem.id } : { idValue: "", value: "" }
 
   if (!attachmentIsAMatchedPR && core.getInput("add_only")) return
-  log(`syncing card: ${card.name}`)
 
-  const customFieldItemsResponse = await trelloFetch(`cards/${card.id}/customFieldItems`)
-  const customFieldItems = await customFieldItemsResponse.json()
+  const customFieldItems = await getCardCustomItemFields(card)
   const alreadyHasEnvironmentSet = customFieldItems.some(
     ({ idCustomField }) => customFieldItem.idCustomField === idCustomField,
   )
   if (alreadyHasEnvironmentSet) {
-    log("already set")
+    log(`syncing card: ${card.name}; already set`)
     return
   }
-  log(attachmentIsAMatchedPR ? "adding" : "removing")
-  return await updateCustomFieldToStaging({ card, customFieldItem, body })
+  log(`syncing card: ${card.name}; ${attachmentIsAMatchedPR ? "adding" : "removing"}`)
+  return await updateCustomField({ card, customFieldItem, body })
 }
 
-async function updateCustomFieldToStaging({ card, customFieldItem, body }) {
-  return await trelloFetch(`cards/${card.id}/customField/${customFieldItem.idCustomField}/item`, {
-    method: "PUT",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify(body),
-  })
-}
-
+// we only get 250 per page so we will iterate over pages to grab more if there is more
 async function findCommitsFromShaToMaster() {
-  const owner = github.context.payload.repository.owner.name
-  const repo = github.context.payload.repository.name
-  const currentSha = github.context.sha
-  const basehead = `master...${currentSha}`
-  const {
-    data: { commits, total_commits },
-  } = await getOctokit().request(`GET /repos/${owner}/${repo}/compare/${basehead}`)
+  const { commits, total_commits } = await getCommitsFromMaster()
   let allCommits = commits
   const extraPagesCount = Math.min(Math.floor(total_commits / 250), 5) // let's cap at 1500 commits
   for (let index = 0; index < extraPagesCount; index++) {
     const page = index + 2 // we already loaded page 1
-    const {
-      data: { commits: pageCommits },
-    } = await getOctokit().request(`GET /repos/${owner}/${repo}/compare/${basehead}`, { page })
-
+    const { commits: pageCommits } = await getCommitsFromMaster({ page })
     allCommits = [...allCommits, ...pageCommits]
   }
 
   return allCommits
 }
 
-// async function getPullRequestsWithCurrentSha() {
-//   const owner = github.context.payload.repository.owner.name
-//   const repo = github.context.payload.repository.name
-//   const currentSha = github.context.sha
-//   return await getOctokit().rest.repos.listPullRequestsAssociatedWithCommit({
-//     owner,
-//     repo,
-//     commit_sha: currentSha,
-//   })
-// }
-
-async function getHeadCommitForPR(id) {
-  const owner = github.context.payload.repository.owner.name
-  const repo = github.context.payload.repository.name
-  const {
-    data: {
-      head: { sha },
-    },
-  } = await getOctokit().request(`GET /repos/${owner}/${repo}/pulls/${id}`)
-  return sha
-}
-
-function getOctokit() {
-  const githubToken = core.getInput("github_token")
-  return github.getOctokit(githubToken)
-}
-
 function isPullRequestAttachment(attachment) {
   const owner = github.context.payload.repository.owner.name
   const repo = github.context.payload.repository.name
   return attachment.url.includes(`github.com/${owner}/${repo}/pull`)
-}
-
-function log(toLog) {
-  if (typeof toLog === "object") {
-    core.info(JSON.stringify(toLog, undefined, 2))
-  } else {
-    core.info(toLog)
-  }
 }
