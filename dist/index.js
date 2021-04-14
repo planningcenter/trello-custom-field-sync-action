56404a633fd2ad945318ee36b2bcd52877cb7016
Kyle Mellander <kylemellander@gmail.com>
2021-04-14 11:17:57 -0700

    clean up testing actions

--- dist/index.js
+++ dist/index.js
@@ -1,96 +1,5 @@
-require('./sourcemap-register.js');module.exports =
-/******/ (() => { // webpackBootstrap
+require('./sourcemap-register.js');/******/ (() => { // webpackBootstrap
 /******/ 	var __webpack_modules__ = ({
-
-/***/ 932:
-/***/ ((__unused_webpack_module, __unused_webpack_exports, __nccwpck_require__) => {
-
-const core = __nccwpck_require__(186)
-const github = __nccwpck_require__(438)
-const { trelloFetch } = __nccwpck_require__(447)
-
-async function run() {
-  try {
-    const stagingCustomFieldItem = await getStagingCustomFieldItem()
-    const filteredCards = await getCardsWithPRAttachments()
-    const { data: pullRequestsOnCurrentSha } = await getPullRequestsWithCurrentSha()
-
-    filteredCards.forEach(async (card) => {
-      setCardCustomFieldValue({
-        card,
-        prs: pullRequestsOnCurrentSha,
-        customFieldItem: stagingCustomFieldItem,
-      })
-    })
-  } catch (error) {
-    core.setFailed(error.message)
-  }
-}
-
-run()
-
-async function getCardsWithPRAttachments() {
-  const response = await trelloFetch(
-    `boards/${core.getInput("trello_board_id")}/cards?attachments=true`,
-  )
-  const cards = await response.json()
-  return cards.filter((card) => card.attachments.some(isPullRequestAttachment))
-}
-
-async function getEnvironmentCustomField() {
-  const response = await trelloFetch(`boards/${core.getInput("trello_board_id")}/customFields`)
-  const customFields = await response.json()
-  return customFields.find(({ name }) => name === core.getInput("trello_custom_field_name"))
-}
-
-async function getStagingCustomFieldItem() {
-  const customField = await getEnvironmentCustomField()
-  return customField.options.find(
-    (option) => Object.values(option.value)[0] === core.getInput("trello_custom_field_value"),
-  )
-}
-
-async function setCardCustomFieldValue({ card, prs, customFieldItem }) {
-  const attachments = card.attachments.filter(isPullRequestAttachment)
-  const attachmentIsAMatchedPR = attachments.some((attachment) => {
-    const prId = parseInt(attachment.url.split("/").pop(), 10)
-    return prs.some((pr) => pr.number === prId)
-  })
-  const body = attachmentIsAMatchedPR ? { idValue: customFieldItem.id } : { idValue: "", value: "" }
-
-  if (!attachmentIsAMatchedPR && core.getInput("add_only")) return
-  return await updateCustomFieldToStaging({ card, customFieldItem, body })
-}
-
-async function updateCustomFieldToStaging({ card, customFieldItem, body }) {
-  return await trelloFetch(`cards/${card.id}/customField/${customFieldItem.idCustomField}/item`, {
-    method: "PUT",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify(body),
-  })
-}
-
-async function getPullRequestsWithCurrentSha() {
-  const owner = github.context.payload.repository.owner.name
-  const repo = github.context.payload.repository.name
-  const currentSha = github.context.sha
-  const githubToken = core.getInput("github_token")
-  const octokit = github.getOctokit(githubToken)
-  return await octokit.rest.repos.listPullRequestsAssociatedWithCommit({
-    owner,
-    repo,
-    commit_sha: currentSha,
-  })
-}
-
-function isPullRequestAttachment(attachment) {
-  const owner = github.context.payload.repository.owner.name
-  const repo = github.context.payload.repository.name
-  return attachment.url.includes(`github.com/${owner}/${repo}/pull`)
-}
-
-
-/***/ }),
 
 /***/ 351:
 /***/ (function(__unused_webpack_module, exports, __nccwpck_require__) {
@@ -6167,8 +6076,9 @@ module.exports = require("zlib");;
 /******/ 	// The require function
 /******/ 	function __nccwpck_require__(moduleId) {
 /******/ 		// Check if module is in cache
-/******/ 		if(__webpack_module_cache__[moduleId]) {
-/******/ 			return __webpack_module_cache__[moduleId].exports;
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
 /******/ 		}
 /******/ 		// Create a new module (and put it into the cache)
 /******/ 		var module = __webpack_module_cache__[moduleId] = {
@@ -6193,11 +6103,97 @@ module.exports = require("zlib");;
 /************************************************************************/
 /******/ 	/* webpack/runtime/compat */
 /******/ 	
-/******/ 	__nccwpck_require__.ab = __dirname + "/";/************************************************************************/
-/******/ 	// module exports must be returned from runtime so entry inlining is disabled
-/******/ 	// startup
-/******/ 	// Load entry module and return exports
-/******/ 	return __nccwpck_require__(932);
+/******/ 	if (typeof __nccwpck_require__ !== 'undefined') __nccwpck_require__.ab = __dirname + "/";/************************************************************************/
+var __webpack_exports__ = {};
+// This entry need to be wrapped in an IIFE because it need to be isolated against other modules in the chunk.
+(() => {
+const core = __nccwpck_require__(186)
+const github = __nccwpck_require__(438)
+const { trelloFetch } = __nccwpck_require__(447)
+
+async function run() {
+  try {
+    const stagingCustomFieldItem = await getStagingCustomFieldItem()
+    const filteredCards = await getCardsWithPRAttachments()
+    const { data: pullRequestsOnCurrentSha } = await getPullRequestsWithCurrentSha()
+
+    filteredCards.forEach(async (card) => {
+      setCardCustomFieldValue({
+        card,
+        prs: pullRequestsOnCurrentSha,
+        customFieldItem: stagingCustomFieldItem,
+      })
+    })
+  } catch (error) {
+    core.setFailed(error.message)
+  }
+}
+
+run()
+
+async function getCardsWithPRAttachments() {
+  const response = await trelloFetch(
+    `boards/${core.getInput("trello_board_id")}/cards?attachments=true`,
+  )
+  const cards = await response.json()
+  return cards.filter((card) => card.attachments.some(isPullRequestAttachment))
+}
+
+async function getEnvironmentCustomField() {
+  const response = await trelloFetch(`boards/${core.getInput("trello_board_id")}/customFields`)
+  const customFields = await response.json()
+  return customFields.find(({ name }) => name === core.getInput("trello_custom_field_name"))
+}
+
+async function getStagingCustomFieldItem() {
+  const customField = await getEnvironmentCustomField()
+  return customField.options.find(
+    (option) => Object.values(option.value)[0] === core.getInput("trello_custom_field_value"),
+  )
+}
+
+async function setCardCustomFieldValue({ card, prs, customFieldItem }) {
+  const attachments = card.attachments.filter(isPullRequestAttachment)
+  const attachmentIsAMatchedPR = attachments.some((attachment) => {
+    const prId = parseInt(attachment.url.split("/").pop(), 10)
+    return prs.some((pr) => pr.number === prId)
+  })
+  const body = attachmentIsAMatchedPR ? { idValue: customFieldItem.id } : { idValue: "", value: "" }
+
+  if (!attachmentIsAMatchedPR && core.getInput("add_only")) return
+  return await updateCustomFieldToStaging({ card, customFieldItem, body })
+}
+
+async function updateCustomFieldToStaging({ card, customFieldItem, body }) {
+  return await trelloFetch(`cards/${card.id}/customField/${customFieldItem.idCustomField}/item`, {
+    method: "PUT",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(body),
+  })
+}
+
+async function getPullRequestsWithCurrentSha() {
+  const owner = github.context.payload.repository.owner.name
+  const repo = github.context.payload.repository.name
+  const currentSha = github.context.sha
+  const githubToken = core.getInput("github_token")
+  const octokit = github.getOctokit(githubToken)
+  return await octokit.rest.repos.listPullRequestsAssociatedWithCommit({
+    owner,
+    repo,
+    commit_sha: currentSha,
+  })
+}
+
+function isPullRequestAttachment(attachment) {
+  const owner = github.context.payload.repository.owner.name
+  const repo = github.context.payload.repository.name
+  return attachment.url.includes(`github.com/${owner}/${repo}/pull`)
+}
+
+})();
+
+module.exports = __webpack_exports__;
 /******/ })()
 ;
 //# sourceMappingURL=index.js.map
