0ac13908e5b076a16de125f683226745d267c8b4
Kyle Mellander <kylemellander@gmail.com>
2021-04-14 08:33:57 -0700

    update specs

--- index.test.js
+++ index.test.js
@@ -4,7 +4,10 @@ const path = require('path');
 
 // shows how the runner will run a javascript action with env / stdout protocol
 test('test runs', () => {
-  process.env['INPUT_MILLISECONDS'] = 500;
+  process.env['INPUT_GITHUB_TOKEN'] = 'notreal';
+  process.env['INPUT_TRELLO_KEY'] = 'updateme';
+  process.env['INPUT_TRELLO_TOKEN'] = 'reallydontwantthistobereal';
+  process.env['GITHUB_SHA'] = 'abcd'
   const ip = path.join(__dirname, 'index.js');
   console.log(cp.execSync(`node ${ip}`, {env: process.env}).toString());
 })
